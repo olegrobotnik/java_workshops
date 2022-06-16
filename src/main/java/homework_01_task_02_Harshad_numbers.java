da754f5b0c184e81035ae886ea8a2a426f64f638
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-06-16 15:36:17 +0300

    Add task condition in task_02_Harshad_numbers

--- src/main/java/homework_01_task_02_Harshad_numbers.java
+++ src/main/java/homework_01_task_02_Harshad_numbers.java
@@ -1,3 +1,5 @@
+//Написать программу вычисления n-ого треугольного числа. https://www.google.com/url?q=http://ru.wikipedia.org/wiki/%25D0%25A2%25D1%2580%25D0%25B5%25D1%2583%25D0%25B3%25D0%25BE%25D0%25BB%25D1%258C%25D0%25BD%25D0%25BE%25D0%25B5_%25D1%2587%25D0%25B8%25D1%2581%25D0%25BB%25D0%25BE&sa=D&source=docs&ust=1655220207346067&usg=AOvVaw3gJTO4FgcibF0Zo_INUoNm
+
 //import java.util.Scanner;   //Use for console input
 
 import java.util.Random;
